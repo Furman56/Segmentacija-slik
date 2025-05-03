6080b8eaad6766dd5a57807d872973cba99222d8
Furman56 <furman.urban@gmail.com>
2025-05-03 16:44:33 +0200

    SCRUM-3: Inicializacija Kmeans funkcije

--- naloga3.py
+++ naloga3.py
@@ -1,8 +1,18 @@
 import cv2 as cv
+import numpy as np
 
 def kmeans(slika, k=3, iteracije=10):
     '''Izvede segmentacijo slike z uporabo metode k-means.'''
-    pass
+    
+    # Pridobi dimenzije slike
+    h, w, d = slika.shape  # h = height, w = width, d = depth (barvni razredi)
+    
+    # Pretvori sliko v 2D matriko kjer je vsaka vrstica en pixel
+    pixels = slika.reshape(-1, d)  # (st_pixlov, depth)
+    
+    # Inicializira naključno st centrov iz pikslov
+    centers = pixels[np.random.choice(pixels.shape[0], k, replace=False)] 
+    
 
 def meanshift(slika, velikost_okna, dimenzija):
     '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
